5f05aca51a3f6017742c2288d88221d7337b3ed7
Hellowlol <hellowlol1@gmail.com>
2017-09-30 00:45:03 +0200

    commit zhe cookie

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for plexcli."""
+
+__author__ = """hellowlol"""
+__email__ = 'hellowlol1@gmail.com'
+__version__ = '0.0.1'
---
+++ cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for plexcli."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for plexcli."""
+    click.echo("Replace this message by putting your code into "
+               "plexcli.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
